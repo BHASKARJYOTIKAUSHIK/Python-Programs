f2dca151d8f669081ddc03f863ab022f89618bac
BHASKARJYOTIKAUSHIK <130184046+BHASKARJYOTIKAUSHIK@users.noreply.github.com>
2024-06-19 23:55:20 +0530

    Update sumofeven.py

--- sumofeven.py
+++ sumofeven.py
@@ -1,1 +1,16 @@
-n=int
+n=int(input("Enter number:"))
+# b=0
+# for i in range(0,n+1):
+#     if i%2==0:
+#         b+=i
+#     else:
+#         pass
+a=0
+for i in range(0,n+1):
+    if i%2==0:
+        pass
+    else:
+        a+=i
+c=n*(n+2)//4
+print(f"Sum of all even numbers till {n} is ",c)
+print(f"Sum of all odd numbers till {n} is ",a)
